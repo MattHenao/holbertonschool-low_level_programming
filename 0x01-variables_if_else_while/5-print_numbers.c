823e081aae62610f26b89a7358a0017f3fcfb051
MattHenao <2340@holbertonschool.com>
2021-02-04 23:44:52 +0000

    Numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print the numbers from 0 to 9
+ *
+ * Return: 0 always runs
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
