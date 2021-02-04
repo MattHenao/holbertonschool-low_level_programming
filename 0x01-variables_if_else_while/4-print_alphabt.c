c045c63b78943679359df17aa2e8df4da1b72ba3
MattHenao <2340@holbertonschool.com>
2021-02-04 23:27:48 +0000

    Alphabet in lowercase without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print the alphabet lowercase except q and e
+ *
+ * Return: 0 always run
+ */
+
+int main(void)
+{
+	char a;
+	for (a = 97; a <= 122; ++a)
+	if (a == '101' || a == '113')
+	{
+		continue;
+	}
+	else
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
