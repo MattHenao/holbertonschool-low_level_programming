057f9e67308548009d8024efa435474208d36ae3
MattHenao <2340@holbertonschool.com>
2021-02-08 17:18:41 +0000

    Print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <holberton.h>
+
+/**
+ * main - Print holberton
+ *
+ * Return: 0 always runs
+ */
+
+int main(void)
+
+{
+	char h[9] = "Holberton";
+	int a;
+
+	for (a = 0; a < 9; i++)
+	{
+		_putchar(h);
+	}
+	return (0);
+}
