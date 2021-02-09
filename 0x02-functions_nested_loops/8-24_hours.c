d38812f75fb8657993a3a18249e0fd6c25af6ebe
MattHenao <2340@holbertonschool.com>
2021-02-09 18:35:06 +0000

    Print the hours of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,17 +10,17 @@ void jack_bauer(void)
 {
 	int a = 0;
 	int b = 0;
-	
+
 	for (a = 0; a < 24; a++)
 	{
 		for (b = 0; b < 60; b++)
-                {
-                        _putchar(a / 10 + '0');
-                        _putchar(a % 10 + '0');
-                        _putchar(':');
-                        _putchar(b / 10 + '0');
-                        _putchar(b % 10 + '0');
-                        _putchar('\n');
-                }
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
 	}
 }
