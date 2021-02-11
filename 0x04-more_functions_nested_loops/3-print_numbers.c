930975005121efeddf184f13381590bb56598303
MattHenao <2340@holbertonschool.com>
2021-02-11 21:26:03 +0000

    Prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	return (0);
+}
