6b648c8b75fff510fb316f80ada8b438c9476dbb
MattHenao <2340@holbertonschool.com>
2021-02-22 21:57:01 +0000

    Copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - Copies memory area
+ *
+ * @dest: Output
+ *
+ * @src: String
+ *
+ * @n: Bytes
+ *
+ * Return: s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[z] = src[z];
+	}
+	return (dest);
+}
