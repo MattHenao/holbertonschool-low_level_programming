6656c58c6c871a1e8362ce4f5040443b1dc1bbdf
MattHenao <2340@holbertonschool.com>
2021-03-01 21:23:24 +0000

    Prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_square - Prints a square
+ *
+ * @size: Size of the square
+ */
+
+void print_square(int size)
+{
+	int a = 35;
+	int b;
+	int c;
+	
+	if (size > 0)
+	{
+		for (b = 0; b < size; b++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar(a);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	
+}
