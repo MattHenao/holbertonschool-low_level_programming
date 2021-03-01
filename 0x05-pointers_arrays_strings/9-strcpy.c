24d0b6fb1a7fccaf521111978aab333e44e077d6
MattHenao <2340@holbertonschool.com>
2021-03-01 22:03:48 +0000

    Copies the string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - Copies the string
+ *
+ * @dest: Destination
+ *
+ * @src: String
+ *
+ * Return: Pointer of dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\n'; a++)
+	{
+		dest[a] = src[a];
+	}
+        _putchar('\n');
+	dest[a] = '\0';
+	return (dest);
+}
