2be11b909ae229328f8d0dba4df8624f26aa9e85
MattHenao <2340@holbertonschool.com>
2021-03-20 15:45:13 +0000

    Adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - Adds two integers and returns the result
+ *
+ * @a: First digit
+ *
+ * @b: Second digit
+ *
+ * Return: Result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
